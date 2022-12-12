c1f07b22c8b5a0fe0711ac150dad5ef4bdcdbf9a
somya ranjan singh <maaheersj@gmail.com>
2022-12-13 00:34:11 +0530

    some files added like middlewares and error file -day-2

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const product = require("./routes/productRoute");
+const errorMiddleware = require("./middleware/error")
 
 app.use(express.json());
 app.use("/api/v1" , product)
+
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ backend/utils/errorhandler.js
@@ -0,0 +1,10 @@
+class Errorhandler extends Error{
+   constructor(message,statuscode){
+    super(message);
+    this.statuscode = statuscode;
+
+    Error.captureStackTrace(this,this.constructor);
+   }
+}
+
+module.exports = Errorhandler;
